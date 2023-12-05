c9fb3a90d7e6c3e0b2375e1f96fbbdc3c819dae5
Jair <jairaceves56@gmail.com>
2023-12-05 16:02:35 -0600

    Configurate Firebase

--- src/main.js
+++ src/main.js
@@ -8,10 +8,19 @@ import config from '../formkit.config'
 import App from './App.vue'
 import router from './router'
 
+// Firebase
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(plugin, defaultConfig(config))
 app.use(router)
+
+app.use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()],
+})
 
 app.mount('#app')
