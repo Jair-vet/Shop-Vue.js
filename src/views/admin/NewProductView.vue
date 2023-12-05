bbe4c0209d886e7ddb99fb633079ca43e6df62ab
Jair <jairaceves56@gmail.com>
2023-12-05 11:55:41 -0600

    Create Routing control from products

---
+++ src/views/admin/NewProductView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Link from '@/components/link.vue';
+
+
+
+</script>
+<template>
+    <div>
+        <Link
+          to="products"
+        >
+          Back
+        </Link>
+      
+        <h1 class="text-4xl font-black text-gray-700 my-10">New Product</h1>
+      
+        <!-- Form -->
+    </div>
+
+</template>
+
+
